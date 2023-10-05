2ba2274935613d679cf3e24fac626d2be2f6d766
alexnesbitt18 <alexnesbitt18@gmail.com>
2023-10-05 13:59:34 -0700

    Completed the Update Product Portion

--- Testing/ProductRepository.cs
+++ Testing/ProductRepository.cs
@@ -13,7 +13,6 @@ namespace Testing
         {
             _conn = conn;
         }
-
         public IEnumerable<Product> GetAllProducts()
         {
             return _conn.Query<Product>("SELECT * FROM PRODUCTS;");
@@ -21,6 +20,11 @@ namespace Testing
         public Product GetProduct(int id)
         {
             return _conn.QuerySingle<Product>("SELECT * FROM PRODUCTS WHERE PRODUCTID = @id", new { id = id });
+        }
+        public void UpdateProduct(Product product)
+        {
+            _conn.Execute("UPDATE products SET Name = @name, Price = @price WHERE ProductID = @id",
+             new { name = product.Name, price = product.Price, id = product.ProductID });
         }
     }
 }
